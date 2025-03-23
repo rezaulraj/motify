be91d5b63d275e04594d67a1f277b8495f34f0b3
Md. Rezaul Karim <121923125+rezaulraj@users.noreply.github.com>
2025-03-23 12:04:02 +0600

    Update db.js

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connect to mongodb ${process.env.HOST}`);
+    console.log(`Connect to mongodb`);
   } catch (error) {
     console.log("Failed to connect to Mongodb", error);
     process.exit(1);
